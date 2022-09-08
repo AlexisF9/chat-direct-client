a8eeec939cbe3759f02098a027d880008f51d1e3
AlexisF9 <alexis.flacher@tbwa-paris.com>
2022-09-08 16:02:07 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Components/Chat";
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
+  const [openRoom, setOpenRoom] = useState(false);
+
+  const joinRoom = (e) => {
+    e.preventDefault();
+    socket.emit("join_room", room);
+    setOpenRoom(true);
+  };
+
+  return (
+    <div>
+      {openRoom ? (
+        <Chat
+          socket={socket}
+          username={username}
+          room={room}
+          setOpenRoom={setOpenRoom}
+        />
+      ) : (
+        <div className="flex flex-col justify-center items-center h-screen w-full">
+          <h1 className="mb-4 text-xl">Join a room</h1>
+          <form onSubmit={joinRoom} className="flex flex-col w-96	">
+            <input
+              className="border-2 p-2"
+              required
+              type="text"
+              placeholder="My pseudo"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              className="border-2 my-4 p-2"
+              required
+              type="text"
+              placeholder="Room ID"
+              value={room}
+              onChange={(e) => setRoom(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="bg-sky-500 hover:bg-sky-700 px-4 py-2 rounded-full text-white"
+            >
+              Join room
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Chat.js
@@ -0,0 +1,90 @@
+import { createRef, useEffect, useRef, useState } from "react";
+
+export default function Chat({ socket, username, room, setOpenRoom }) {
+  const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const messageEndRef = createRef();
+
+  const _isInit = useRef(false);
+
+  useEffect(() => {
+    if (!_isInit.current) {
+      _isInit.current = true;
+
+      socket.on("receive_message", (data) => {
+        setMessageList((list) => [...list, data]);
+      });
+    }
+  }, [socket]);
+
+  useEffect(() => {
+    messageEndRef.current.scrollIntoView();
+  }, [messageList]);
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const messageData = {
+      room: room,
+      author: username,
+      message: message,
+      time:
+        new Date(Date.now()).getHours() +
+        ":" +
+        new Date(Date.now()).getMinutes(),
+    };
+    await socket.emit("send_message", messageData);
+    setMessageList((list) => [...list, messageData]);
+  };
+
+  const closeRoom = () => {
+    socket.emit("leave_room", room);
+    setOpenRoom(false);
+  };
+
+  return (
+    <div className="ml-8 mt-8">
+      <button
+        className="bg-red-500	hover:bg-red-700 px-4 py-2 rounded-full text-white"
+        onClick={() => closeRoom()}
+      >
+        Close room
+      </button>
+      <h3 className="my-4">
+        Welcome <strong>{username}</strong> in the room :{" "}
+        <strong>{room}</strong>
+      </h3>
+
+      <div className="pb-6 px-2 w-96 h-96 bg-slate-200 overflow-y-scroll">
+        {messageList.map((item, i) => {
+          return (
+            <div key={i} className={item.author === username && "text-sky-500"}>
+              <p className="m-2">
+                {item.author} : <strong>{item.message}</strong> ({item.time})
+              </p>
+            </div>
+          );
+        })}
+        <div ref={messageEndRef} />
+      </div>
+
+      <form onSubmit={sendMessage} className="flex flex-col w-96	">
+        <input
+          className="border-2 my-4 p-2"
+          type="text"
+          placeholder="Your message"
+          required
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button
+          className="bg-sky-500 hover:bg-sky-700 px-4 py-2 rounded-full text-white"
+          type="submit"
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  );
+}
